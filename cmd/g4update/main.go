51cee6e2ef579ef895d6cf57f06c804338a34c6e
Eric Cooper <ecc@cmu.edu>
2018-01-31 21:34:01 -0500

    Add g4update program

---
+++ cmd/g4update/main.go
@@ -0,0 +1,209 @@
+package main
+
+// Fetch recent CGM readings from a Dexcom G4 receiver,
+// with options to upload to Nightscout and update a local JSON file.
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ecc1/dexcom"
+	"github.com/ecc1/nightscout"
+	"github.com/ecc1/papertrail"
+)
+
+type (
+	// Entries is an alias, for conciseness.
+	Entries = nightscout.Entries
+)
+
+const (
+	maxClockDelta = 5 * time.Minute
+	gapDuration   = 7 * time.Minute
+)
+
+var (
+	cgmHistory         = flag.Duration("b", 20*time.Minute, "maximum age of CGM entries to fetch")
+	uploadFlag         = flag.Bool("u", false, "upload to Nightscout")
+	simulateUploadFlag = flag.Bool("s", false, "simulate upload to Nightscout")
+	verboseFlag        = flag.Bool("v", false, "verbose mode")
+	jsonFile           = flag.String("f", "", "append results to JSON `file`")
+	jsonCutoff         = flag.Duration("k", 7*24*time.Hour, "maximum age of CGM entries to keep in JSON file")
+
+	cgm            *dexcom.CGM
+	cgmTime        time.Time
+	cgmEpoch       time.Time
+	glucose        dexcom.Records
+	cgmRecords     dexcom.Records
+	reverseEntries Entries
+	forwardEntries Entries
+
+	somethingFailed = false
+)
+
+func main() {
+	flag.Parse()
+	if *simulateUploadFlag {
+		*uploadFlag = true
+	}
+	nightscout.SetNoUpload(*simulateUploadFlag)
+	nightscout.SetVerbose(*verboseFlag)
+	papertrail.StartLogging()
+	getCGMInfo()
+	if *verboseFlag && !*uploadFlag {
+		forwardEntries.Print()
+	}
+	if *jsonFile != "" {
+		appendJSON()
+	}
+	if *uploadFlag {
+		uploadEntries()
+	}
+	if somethingFailed {
+		os.Exit(1)
+	}
+}
+
+func getCGMInfo() {
+	cgm = dexcom.Open()
+	if cgm.Error() != nil {
+		log.Fatal(cgm.Error())
+	}
+	cgmTime = checkCGMClock()
+	cgmEpoch = cgmTime.Add(-*cgmHistory)
+	sensor := cgm.ReadHistory(dexcom.SensorData, cgmEpoch)
+	egv := cgm.ReadHistory(dexcom.EGVData, cgmEpoch)
+	meter := cgm.ReadHistory(dexcom.MeterData, cgmEpoch)
+	cal := cgm.ReadHistory(dexcom.CalibrationData, cgmEpoch)
+	if cgm.Error() != nil {
+		log.Fatal(cgm.Error())
+	}
+	glucose = validateGlucose(egv)
+	if *verboseFlag {
+		log.Printf("%d valid glucose records", len(glucose))
+	}
+	cgmRecords = dexcom.MergeHistory(sensor, glucose, meter, cal)
+	reverseEntries = discardIncomplete(dexcom.NightscoutEntries(cgmRecords))
+	forwardEntries = reverseEntries.Sort()
+	log.Printf("%d CGM records", len(cgmRecords))
+	describeEntries(forwardEntries, "Nightscout")
+}
+
+func timeStr(e nightscout.Entry) string {
+	return e.Time().Format(dexcom.UserTimeLayout)
+}
+
+func describeEntries(v Entries, kind string) {
+	n := len(v)
+	switch n {
+	case 0:
+		log.Printf("0 %s entries", kind)
+	case 1:
+		log.Printf("1 %s entry at %s", kind, timeStr(v[0]))
+	default:
+		log.Printf("%d %s entries from %s to %s", n, kind, timeStr(v[0]), timeStr(v[n-1]))
+	}
+}
+
+func validateGlucose(readings dexcom.Records) dexcom.Records {
+	valid := make(dexcom.Records, 0, len(readings))
+	for _, r := range readings {
+		if !dexcom.IsSpecial(r.Glucose()) {
+			valid = append(valid, r)
+		}
+	}
+	return valid
+}
+
+func uploadEntries() {
+	gaps, err := nightscout.Gaps(cgmEpoch, gapDuration)
+	if err != nil {
+		log.Print(err)
+		somethingFailed = true
+		return
+	}
+	if *verboseFlag {
+		printGaps(gaps)
+	}
+	if len(gaps) == 0 {
+		log.Printf("no Nightscout gaps")
+		return
+	}
+	missing := nightscout.Missing(reverseEntries, gaps)
+	log.Printf("uploading %d entries to Nightscout", len(missing))
+	for _, e := range missing {
+		err := nightscout.Upload("POST", "entries", e)
+		if err != nil {
+			log.Print(err)
+			somethingFailed = true
+			return
+		}
+	}
+}
+
+// If the most recent glucose entry is incomplete, discard it.
+// This can happen if the loop runs at the same time the sensor
+// transmits a new reading, or if the sensor is warming up. If we
+// simply wait until next time, both EGV and sensor records might be
+// available.  If the sensor is warming up, there still won't be a
+// matching EGV record, but the raw-only entry will be uploaded
+// because it's no longer the most recent.
+func discardIncomplete(entries Entries) Entries {
+	e := entries[0]
+	if e.Type == nightscout.SGVType && (e.SGV == 0 || e.Unfiltered == 0) {
+		return entries[1:]
+	}
+	return entries
+}
+
+func checkCGMClock() time.Time {
+	t := cgm.ReadDisplayTime()
+	if cgm.Error() != nil {
+		return t
+	}
+	delta := time.Until(t)
+	if delta < 0 {
+		delta = -delta
+	}
+	log.Printf("CGM clock difference = %v", delta)
+	if delta > maxClockDelta {
+		cgm.SetError(fmt.Errorf("CGM clock difference is greater than %v", maxClockDelta))
+	}
+	return t
+}
+
+func printGaps(gaps []nightscout.Gap) {
+	for _, g := range gaps {
+		t1 := g.Start
+		t2 := g.Finish
+		gap := t2.Sub(t1)
+		s1 := t1.Format(dexcom.UserTimeLayout)
+		s2 := t2.Format(dexcom.UserTimeLayout)
+		log.Printf("%v gap from %s to %s", gap, s1, s2)
+	}
+}
+
+func appendJSON() {
+	previous, err := nightscout.ReadEntries(*jsonFile)
+	if err != nil && !os.IsNotExist(err) {
+		log.Print(err)
+		somethingFailed = true
+		return
+	}
+	log.Printf("read %d entries from %s", len(previous), *jsonFile)
+	previous.Sort()
+	log.Printf("merging %d old and %d new entries", len(previous), len(forwardEntries))
+	merged := nightscout.MergeEntries(previous, forwardEntries)
+	describeEntries(merged, "merged")
+	cutoff := cgmTime.Add(-*jsonCutoff)
+	trimmed := merged.TrimAfter(cutoff)
+	describeEntries(trimmed, "trimmed")
+	err = trimmed.Save(*jsonFile)
+	if err != nil {
+		log.Print(err)
+		somethingFailed = true
+	}
+}
