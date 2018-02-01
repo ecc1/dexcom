4844fa750ef356983f36e2c1875e3c72abd1bf06
Eric Cooper <ecc@cmu.edu>
2018-01-31 21:33:37 -0500

    Add papertrail logging

--- cmd/g4ping/main.go
+++ cmd/g4ping/main.go
@@ -4,9 +4,11 @@ import (
 	"log"
 
 	"github.com/ecc1/dexcom"
+	"github.com/ecc1/papertrail"
 )
 
 func main() {
+	papertrail.StartLogging()
 	cgm := dexcom.Open()
 	cgm.Cmd(dexcom.Ping)
 	if cgm.Error() != nil {
